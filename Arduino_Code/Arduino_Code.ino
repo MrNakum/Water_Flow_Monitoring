b50a43b3a8bd29a098302864f50872dec78bf82e
PY_LIN_ <urvishmnakum@gmail.com>
2018-09-04 15:57:52 +0530

    Add files via upload

--- Arduino_Code/Arduino_Code.ino
+++ Arduino_Code/Arduino_Code.ino
@@ -1,19 +1,41 @@
+#include <SoftwareSerial.h>
+
+// arduino RX pin=2  arduino TX pin=3
+// connect the arduino RX pin to esp8266 module TX pin   -  connect the arduino TX pin to esp8266 module RX pin
+SoftwareSerial espSerial =  SoftwareSerial(2, 3);
+
+String apiKey = "3TAU2UJ268GI5MLJ";
+String ssid = "WIFI NAME" ;
+String password = "WIFI PASSWORD "  ;
+boolean DEBUG = true;
 byte statusLed    = 13;
 byte sensorInterrupt = 0;  // 0 = digital pin 2
 byte sensorPin       = 2;
 float calibrationFactor = 4.5;
-
 volatile byte pulseCount;
-
 float flowRate;
 unsigned int flowMilliLitres;
 unsigned long totalMilliLitres;
-
 unsigned long oldTime;
 
-void setup()
-{
-  Serial.begin(38400);
+void setup() {
+  // enable debug serial
+  DEBUG = true;
+  Serial.begin(9600);
+  espSerial.begin(115200);
+
+  // Enable this line to reset the module;
+  //espSerial.println("AT+RST");
+  //showResponse(1000);
+
+  // Enable this line to set esp8266 serial speed to 9600 bps
+  //espSerial.println("AT+UART_CUR=9600,8,1,0,0");
+  //showResponse(1000);
+
+  espSerial.println("AT+CWMODE=1");   // set esp8266 as client
+  espSerial.println("AT+CWJAP=\"" + ssid + "\",\"" + password + "\""); // set your home router SSID and password
+  if (DEBUG)  Serial.println("Setup completed");
+
   pinMode(statusLed, OUTPUT);
   pinMode(sensorPin, INPUT);
   digitalWrite(statusLed, HIGH);
@@ -28,11 +50,10 @@ void setup()
   attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
 }
 
-/**
-   Main program loop
-*/
-void loop()
-{
+
+
+// ====================================================================== loop
+void loop() {
 
   if ((millis() - oldTime) > 1000)   // Only process counters once per second
   {
@@ -45,31 +66,27 @@ void loop()
 
     unsigned int frac;
 
-    Serial.print("Flow rate: ");
-    Serial.print(int(flowRate));  // Print the integer part of the variable
-    Serial.print(".");             // Print the decimal point
-    frac = (flowRate - int(flowRate)) * 10;
-    Serial.print(frac, DEC) ;      // Print the fractional part of the variable
-    Serial.print("L/min");
-    Serial.print("  Current Liquid Flowing: ");             // Output separator
-    Serial.print(flowMilliLitres);
-    Serial.print("mL/Sec");
-
-    Serial.print("  Output Liquid Quantity: ");             // Output separator
-    Serial.print(totalMilliLitres);
-    Serial.println("mL");
-
+    //    Serial.print("Flow rate: ");
+    //    Serial.print(int(flowRate));  // Print the integer part of the variable
+    //    Serial.print(".");             // Print the decimal point
+    //    frac = (flowRate - int(flowRate)) * 10;
+    //    Serial.print(frac, DEC) ;      // Print the fractional part of the variable
+    //    Serial.print("L/min");
+    //    Serial.print("  Current Liquid Flowing: ");             // Output separator
+    //    Serial.print(flowMilliLitres);
+    //    Serial.print("mL/Sec");
+    //
+    //    Serial.print("  Output Liquid Quantity: ");             // Output separator
+    //    Serial.print(totalMilliLitres);
+    //    Serial.println("mL");
+    //
+    thingSpeakWrite(flowMilliLitres, totalMilliLitres);
     pulseCount = 0;
 
     attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
   }
 }
 
-/*
-  Insterrupt Service Routine
-*/
-void pulseCounter()
-{
-  // Increment the pulse counter
-  pulseCount++;
-}
+
+
+
