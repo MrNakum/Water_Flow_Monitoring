a2f00caafe60da1c67d179d3145450ce779aec41
PY_LIN_ <urvishmnakum@gmail.com>
2018-09-07 11:10:17 +0530

    Add files via upload

---
+++ Water_Flow_Monitoring/Water_Flow_Monitoring_Arduino/Water_Flow_Monitoring_Arduino.ino
@@ -0,0 +1,77 @@
+////////////////////////////use RX TX pin of both the modules///////////////
+//connection arduino RX to ESP TX and  arduino TX to ESP RX
+
+byte statusLed    = 13;
+
+byte sensorInterrupt = 0;  // 0 = digital pin 2
+byte sensorPin       = 2;
+
+// The hall-effect flow sensor outputs approximately 4.5 pulses per second per
+// litre/minute of flow.
+float calibrationFactor = 4.5;
+
+volatile byte pulseCount;  
+
+float flowRate;
+unsigned int flowMilliLitres;
+unsigned long totalMilliLitres;
+
+unsigned long oldTime;
+///////////////////////////
+///////////////////////////
+String flowrate;
+String Sep1 = ",";
+///////////////////////////
+//////////////////////////
+
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(statusLed, OUTPUT);
+  pinMode(sensorPin, INPUT);
+  digitalWrite(statusLed, HIGH);
+  digitalWrite(sensorPin, HIGH);
+
+  pulseCount        = 0;
+  flowRate          = 0.0;
+  flowMilliLitres   = 0;
+  totalMilliLitres  = 0;
+  oldTime           = 0;
+
+  attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
+}
+
+/**
+   Main program loop
+*/
+void loop()
+{
+
+  if ((millis() - oldTime) > 1000)   // Only process counters once per second
+  {
+    detachInterrupt(sensorInterrupt);
+    flowRate = ((1000.0 / (millis() - oldTime)) * pulseCount) / calibrationFactor;
+    oldTime = millis();
+    flowMilliLitres = (flowRate / 60) * 1000;
+    totalMilliLitres += flowMilliLitres;
+
+    unsigned int frac;
+    frac = (flowRate - int(flowRate)) * 10;
+    String Sep = "." ;
+    flowrate = flowRate + Sep + frac ;
+   
+    String FLowData = flowRate + Sep1 + flowMilliLitres + Sep1 + totalMilliLitres;
+    Serial.println(FLowData);
+    pulseCount = 0;
+    attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
+  }
+}
+
+/*
+  Insterrupt Service Routine
+*/
+void pulseCounter()
+{
+  // Increment the pulse counter
+  pulseCount++;
+}
---
+++ Water_Flow_Monitoring/Water_Flow_Monitoring_Node_Mcu/Water_Flow_Monitoring_Node_Mcu.ino
@@ -0,0 +1,94 @@
+#include <ESP8266WiFi.h>
+
+String apiKey = "3TAU2UJ268GI5MLJ";     //  Enter your Write API key from ThingSpeak
+
+const char *ssid =  "PY_LIN_";     // replace with your wifi ssid and wpa2 key
+const char *pass =  "ironman10";
+const char* server = "api.thingspeak.com";
+WiFiClient client;
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, pass);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+}
+
+void loop()
+{
+
+  if (Serial.available() > 0)
+  {
+    String flowrate = Serial.readStringUntil (',');
+    Serial.read();
+
+    String CurrentLiquidFlowing = Serial.readStringUntil (',');
+    Serial.read();
+
+    String OutLiquidQuantity = Serial.readStringUntil ('\0');
+    //////////////////////////////////////////////////////////
+    ////////Manual testing loop///////////////////////////////////
+
+    //    Serial.print( flowrate );
+    //    Serial.print( '\n' );
+    //    Serial.print ( CurrentLiquidFlowing );
+    //    Serial.print( '\n' );
+    //    Serial.print (OutLiquidQuantity);
+    //    Serial.print('\n' );
+    //    if (isnan(flowrate) || isnan(CurrentLiquidFlowing) || isnan(OutLiquidQuantity) )
+    //    {
+    //      Serial.println("Failed to read from DHT sensor!");
+    //      return;
+    //    }
+
+    if (client.connect(server, 80))  //   "184.106.153.149" or api.thingspeak.com
+    {
+
+      String postStr = apiKey;
+      postStr += "&field1=";
+      postStr += String(flowrate);
+      postStr += "&field2=";
+      postStr += String(CurrentLiquidFlowing);
+      postStr += "&field3=";
+      postStr += String(OutLiquidQuantity);
+      postStr += "\r\n\r\n";
+
+      client.print("POST /update HTTP/1.1\n");
+      client.print("Host: api.thingspeak.com\n");
+      client.print("Connection: close\n");
+      client.print("X-THINGSPEAKAPIKEY: " + apiKey + "\n");
+      client.print("Content-Type: application/x-www-form-urlencoded\n");
+      client.print("Content-Length: ");
+      client.print(postStr.length());
+      client.print("\n\n");
+      client.print(postStr);
+
+      //      Serial.print("Temperature: ");
+      //      Serial.print(flowrate);
+      //      Serial.print(" degrees Celcius, Humidity: ");
+      //      Serial.print(CurrentLiquidFlowing);
+      Serial.println("%. Send to Thingspeak.");
+    }
+    client.stop();
+
+    Serial.println("Waiting...");
+
+    // thingspeak needs minimum 15 sec delay between updates, i've set it to 30 seconds
+    delay(10000);
+
+
+    /////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////
+  }
+}
+
+
+
